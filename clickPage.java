f9458c8da66c0e197e5c4bc7b31afddefa11c4ed
TarakaPhaneendra <141845793+TarakaPhaneendra@users.noreply.github.com>
2026-09-22 07:04:46 +0530

    second commit (#1)
    * second commit
    
    * changed code

---
+++ clickPage.java
@@ -0,0 +1,13 @@
+package pageClasses;
+
+import org.openqa.selenium.WebDriver;
+
+import basePage.BasePage;
+
+public class clickPage extends BasePage{
+WebDriver driver;
+public clickPage(WebDriver driver) {
+	super(driver); //   taraka comment
+
+}
+}
